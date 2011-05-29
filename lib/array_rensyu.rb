97707e5f6ade33c7d70ed6849a22eecab2b4e694
Noriaki Koyanagi <koyanagn@gmail.com>
2011-05-29 14:24:30 +0900

    P190

--- lib/array_rensyu.rb
+++ lib/array_rensyu.rb
@@ -17,8 +17,17 @@ p a
 a = Array.new(5,0)
 p a
 
+#文字列の配列を作る。　%wで。
 lang = %w(Ruby Perl Python Scheme Pike REBOL)
 p lang
 
 iroha = %w(い ろ は に ほ へ と)
 p iroha
+
+#ハッシュテーブルから配列の配列を作成
+color_table ={"black" => "#000000", "white" => "#FFFFFF"}
+p color_table.to_a
+
+#splitメソッドで文字列から配列を作成する。
+column = "2010/01/09 22:33 foo.html proxy.example.jp".split()
+p column
